927bab0e1dcca61b3c71248d4a3cbc85c3a5105c
Nelson <nelson.quispe@salamancasolutions.com>
2020-01-21 20:38:36 +0000

    Init Proyect

---
+++ src/main/java/com/gqlspqer/GqlspqrApplication.java
@@ -0,0 +1,13 @@
+package com.gqlspqer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqlspqrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GqlspqrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gqlspqer/GqlspqrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gqlspqer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GqlspqrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
